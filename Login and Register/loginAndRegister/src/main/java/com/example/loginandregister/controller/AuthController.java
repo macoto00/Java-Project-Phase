2c21427e6cc662e6a91fbfe7186e234b56725edd
TomasMacoun <macount@seznam.cz>
2023-06-27 15:35:38 +0200

    Created endpotÂint to create a user after registration

--- Login and Register/loginAndRegister/src/main/java/com/example/loginandregister/controller/AuthController.java
+++ Login and Register/loginAndRegister/src/main/java/com/example/loginandregister/controller/AuthController.java
@@ -1,12 +1,55 @@
 package com.example.loginandregister.controller;
 
+import com.example.loginandregister.dto.UserDto;
+import com.example.loginandregister.model.User;
+import com.example.loginandregister.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
     }
 }
