b54609ac639fa5de514560015629cc2c480db49b
TomasMacoun <macount@seznam.cz>
2023-06-28 12:41:53 +0200

    initial commit

---
+++ JWT_Authentication_and_Authorisation_Maven/src/main/java/com/example/jwt_authentication_and_authorisation_maven/JwtAuthenticationAndAuthorisationMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authentication_and_authorisation_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAndAuthorisationMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationAndAuthorisationMavenApplication.class, args);
+	}
+
+}
