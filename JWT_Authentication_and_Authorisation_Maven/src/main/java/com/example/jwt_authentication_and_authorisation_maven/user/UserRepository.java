e897a5231341694d1f23c0887034f099ce3ad7ca
TomasMacoun <macount@seznam.cz>
2023-06-28 13:05:24 +0200

    Created UseRepository

---
+++ JWT_Authentication_and_Authorisation_Maven/src/main/java/com/example/jwt_authentication_and_authorisation_maven/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.jwt_authentication_and_authorisation_maven.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
