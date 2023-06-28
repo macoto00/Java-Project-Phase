c22386546d0381e786bd7fd6a91ad6f9d3ace561
TomasMacoun <macount@seznam.cz>
2023-06-28 12:31:43 +0200

    updated structure

--- Login and Register (custom)/loginAndRegister/src/main/java/com/example/loginandregister/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.loginandregister.repository;
-
-import com.example.loginandregister.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Java-Project-Phase
 
 Repository of my projects. This is my Java-Learning Journey.
+
+- Login and Registration simple Web Page
