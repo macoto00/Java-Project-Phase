f26d064738f6801ec63e919d3e9e027f8a7cfe0f
TomasMacoun <macount@seznam.cz>
2023-06-28 15:10:22 +0200

    Create AuthenticationResponse.java

---
+++ JWT_Authentication_and_Authorisation_Maven/src/main/java/com/example/jwt_authentication_and_authorisation_maven/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.jwt_authentication_and_authorisation_maven.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
