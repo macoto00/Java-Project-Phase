e2cb96b3ce9fbfdece894e752740bdcf5774a893
TomasMacoun <macount@seznam.cz>
2023-06-28 15:10:18 +0200

    Create AuthenticationRequest.java

---
+++ JWT_Authentication_and_Authorisation_Maven/src/main/java/com/example/jwt_authentication_and_authorisation_maven/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.example.jwt_authentication_and_authorisation_maven.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
