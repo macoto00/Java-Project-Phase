79acd0ef3cc6f5a5f0d39fcff9d9b342039bf102
TomasMacoun <macount@seznam.cz>
2023-06-29 15:07:08 +0200

    created models

---
+++ Spring_Boot_Security_JWT_Authentication_Gradle/src/main/java/com/example/spring_boot_security_jwt_authentication_gradle/models/Role.java
@@ -0,0 +1,17 @@
+package com.example.spring_boot_security_jwt_authentication_gradle.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
