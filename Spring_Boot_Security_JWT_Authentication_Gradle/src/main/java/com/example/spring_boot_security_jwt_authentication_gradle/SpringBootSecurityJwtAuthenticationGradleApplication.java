875cdbc33fe4385eb143b0beb4eb73239848cae6
TomasMacoun <macount@seznam.cz>
2023-06-29 15:08:21 +0200

    Updated structure

---
+++ Spring_Boot_Security_JWT_Authentication_Gradle/src/main/java/com/example/spring_boot_security_jwt_authentication_gradle/SpringBootSecurityJwtAuthenticationGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_security_jwt_authentication_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtAuthenticationGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtAuthenticationGradleApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Security_JWT_Authentication_Gradle/src/test/java/com/example/spring_boot_security_jwt_authentication_gradle/SpringBootSecurityJwtAuthenticationGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_security_jwt_authentication_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJwtAuthenticationGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
