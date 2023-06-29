24ef0ee77c5768e9c09b02f6765f83c80c62029c
TomasMacoun <macount@seznam.cz>
2023-06-29 15:07:22 +0200

    Create AuthController.java

---
+++ Spring_Boot_Security_JWT_Authentication_Gradle/src/main/java/com/example/spring_boot_security_jwt_authentication_gradle/controllers/AuthController.java
@@ -0,0 +1,74 @@
+package com.example.spring_boot_security_jwt_authentication_gradle.controllers;
+
+import com.example.spring_boot_security_jwt_authentication_gradle.dto.AuthResponseDTO;
+import com.example.spring_boot_security_jwt_authentication_gradle.dto.LoginDto;
+import com.example.spring_boot_security_jwt_authentication_gradle.dto.RegisterDto;
+import com.example.spring_boot_security_jwt_authentication_gradle.models.Role;
+import com.example.spring_boot_security_jwt_authentication_gradle.models.UserEntity;
+import com.example.spring_boot_security_jwt_authentication_gradle.repository.RoleRepository;
+import com.example.spring_boot_security_jwt_authentication_gradle.repository.UserRepository;
+import com.example.spring_boot_security_jwt_authentication_gradle.security.JWTGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTGenerator jwtGenerator;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
+                          RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTGenerator jwtGenerator) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        UserEntity user = new UserEntity();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode((registerDto.getPassword())));
+
+        Role roles = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully!", HttpStatus.OK);
+    }
+}
