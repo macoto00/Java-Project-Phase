64f880cf8db9b9338909aef295ad737c954fd2dc
TomasMacoun <macount@seznam.cz>
2023-06-29 21:55:01 +0200

    initial commit

---
+++ Spring_Boot_Unit_Testing_With_Mockito_Gradle/src/main/java/com/example/spring_boot_unit_testing_with_mockito_gradle/SpringBootUnitTestingWithMockitoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_unit_testing_with_mockito_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestingWithMockitoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestingWithMockitoGradleApplication.class, args);
+	}
+
+}
