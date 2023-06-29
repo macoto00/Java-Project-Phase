025c2e82ffb480271b60965f3be00f332cb75e09
TomasMacoun <macount@seznam.cz>
2023-06-29 21:55:26 +0200

    service tests

---
+++ Spring_Boot_Unit_Testing_With_Mockito_Gradle/src/test/java/com/example/spring_boot_unit_testing_with_mockito_gradle/SpringBootUnitTestingWithMockitoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_unit_testing_with_mockito_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestingWithMockitoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
