4c8a585c28810e5006d8446c5b892dc66b8421e2
TomasMacoun <macount@seznam.cz>
2023-06-30 10:38:19 +0200

    created models

---
+++ Spring_Boot_Unit_Testing_With_Mockito_Gradle/src/main/java/com/example/spring_boot_unit_testing_with_mockito_gradle/models/Role.java
@@ -0,0 +1,17 @@
+package com.example.spring_boot_unit_testing_with_mockito_gradle.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
