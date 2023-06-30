a0cb63b1239a05881cd12ac210903c1d1dc9bae7
TomasMacoun <macount@seznam.cz>
2023-06-30 10:10:47 +0200

    Create LoginDto.java

---
+++ Spring_Boot_Unit_Testing_With_Mockito_Gradle/src/main/java/com/example/spring_boot_unit_testing_with_mockito_gradle/dtos/LoginDto.java
@@ -0,0 +1,9 @@
+package com.example.spring_boot_unit_testing_with_mockito_gradle.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
